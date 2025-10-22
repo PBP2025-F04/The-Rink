8810d1df0d3892b8f87322683eaf8d89239d13dd
Rieeel <ldepuuwu14@gmail.com>
2025-10-22 16:30:45 +0700

    commit

--- authentication/views.py
+++ authentication/views.py
@@ -1,42 +1,4 @@
 from django.shortcuts import render, redirect
-<<<<<<< HEAD
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
-
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            next_url = request.GET.get('next', '/')
-            return redirect(next_url)
-        else:
-            messages.error(request, 'Invalid username or password')
-    
-    return render(request, 'authentication/login.html')
-
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, 'Registration successful')
-            return redirect('/')
-        else:
-            messages.error(request, 'Registration failed')
-    else:
-        form = UserCreationForm()
-    
-    return render(request, 'authentication/register.html', {'form': form})
-
-def logout_view(request):
-    logout(request)
-    return redirect('/')
-=======
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
@@ -72,4 +34,3 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return redirect('main:login')
->>>>>>> d4093bfe0e5ab84a85d77ef2323fd24229824147
