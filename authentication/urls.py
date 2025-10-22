3136b3fe3b14f176cb4051b80fbe14a8a8513120
Yalifuddin <yalif1710@gmail.com>
2025-10-22 14:32:42 +0700

    Test login

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,14 +1,4 @@
 from django.urls import path
-<<<<<<< HEAD
-from . import views
-
-app_name = 'authentication'
-
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register, name='register'),
-=======
 from authentication.views import register, login_user, logout_user
 
 app_name = 'main'
@@ -17,5 +7,4 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
->>>>>>> d4093bfe0e5ab84a85d77ef2323fd24229824147
 ]
