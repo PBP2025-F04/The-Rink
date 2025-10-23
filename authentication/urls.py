301b56f9b6852cf5d83194cb8875f85b4ae834b8
Aforambis <rusydanmujtabaa@gmail.com>
2025-10-23 10:42:52 +0700

    Um

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from authentication.views import register, login_user, logout_user
 
-<<<<<<< HEAD
 app_name = 'authentication'
-=======
-app_name = 'main'
->>>>>>> origin
 
 urlpatterns = [
     path('register/', register, name='register'),
