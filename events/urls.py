68c71ef3adc996e8080754edcbac656b6895fa35
Aforambis <rusydanmujtabaa@gmail.com>
2025-10-23 20:29:19 +0700

    Test

--- events/urls.py
+++ events/urls.py
@@ -9,9 +9,18 @@ urlpatterns = [
     path('my-events/', views.my_events, name='my_events'),
     path('<slug:slug>/', views.event_detail, name='detail'),
     
+<<<<<<< HEAD
     # --- HTMX Endpoints ---
     # GET untuk memuat modal
     path('<slug:slug>/get-modal/', views.get_registration_modal, name='get_modal'), 
+=======
+    # e.g., /packages/5/book/ (URL ini untuk GET modal dan POST form)
+    path('packages/<int:package_id>/book/', views.book_package_view, name='book_package'),
+
+    # --- Event ---
+    # e.g., /events/
+    path('', views.event_list_view, name='event_list'),
+>>>>>>> d5c473d (Test)
     
     # POST untuk mendaftar
     path('<slug:slug>/register/', views.register_event, name='register'),
