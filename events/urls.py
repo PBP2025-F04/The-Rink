f7ffe990bec419604ac6dcdf049133cb49ad1418
Aforambis <rusydanmujtabaa@gmail.com>
2025-10-23 21:43:34 +0700

    Features

--- events/urls.py
+++ events/urls.py
@@ -8,19 +8,21 @@ urlpatterns = [
     path('', views.event_list, name='list'),
     path('my-events/', views.my_events, name='my_events'),
     path('<slug:slug>/', views.event_detail, name='detail'),
-    
-<<<<<<< HEAD
+
     # --- HTMX Endpoints ---
     # GET untuk memuat modal
     path('<slug:slug>/get-modal/', views.get_registration_modal, name='get_modal'), 
-=======
+
     # e.g., /packages/5/book/ (URL ini untuk GET modal dan POST form)
     path('packages/<int:package_id>/book/', views.book_package_view, name='book_package'),
 
     # --- Event ---
     # e.g., /events/
     path('', views.event_list_view, name='event_list'),
->>>>>>> d5c473d (Test)
+
+    # --- HTMX Endpoints ---
+    # GET untuk memuat modal
+    path('<slug:slug>/get-modal/', views.get_registration_modal, name='get_modal'), 
     
     # POST untuk mendaftar
     path('<slug:slug>/register/', views.register_event, name='register'),
