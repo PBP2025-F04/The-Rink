36ee67e1c679191869f7b95f52127d8d24463001
Yalifuddin <yalif1710@gmail.com>
2025-12-20 20:52:40 +0700

    sebelum deploy pws

--- the_rink/settings.py
+++ the_rink/settings.py
@@ -68,7 +68,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'the_rink.urls'
